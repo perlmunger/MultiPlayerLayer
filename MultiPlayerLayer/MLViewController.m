c469fad09613f321f04b67aac4dc747d1c12e084
Matt Long <matt.long@matthew-long.com>
2014-02-17 16:11:12 -0700

    Add replicator and player layer code

--- MultiPlayerLayer/MLViewController.m
+++ MultiPlayerLayer/MLViewController.m
@@ -10,6 +10,9 @@
 
 @interface MLViewController ()
 
+@property (nonatomic, strong) AVPlayer *player;
+@property (nonatomic, strong) AVPlayerLayer *playerLayer;
+@property (nonatomic, strong) CAReplicatorLayer *replicatorLayer;
 @end
 
 @implementation MLViewController
@@ -17,13 +20,32 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+    
+    _player = [AVPlayer playerWithURL:[[NSBundle mainBundle] URLForResource:@"Launch" withExtension:@"mp4"]];
+    
+    _playerLayer = [AVPlayerLayer layer];
+    
+    _replicatorLayer = [CAReplicatorLayer layer];
+    [_replicatorLayer setAnchorPoint:CGPointMake(0.0f, 0.0f)];
+    [_replicatorLayer setBounds:CGRectMake(0.0f, 0.0f, 700.0f, 300.0f)];
+    [_replicatorLayer setPosition:CGPointMake(0.0f, 44.0f)];
+    [_replicatorLayer setInstanceCount:8];
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DScale(transform, 0.75, 0.75, 1.0);
+    transform = CATransform3DTranslate(transform, 30.0f, 300.0f, 0.0f);
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [_replicatorLayer setInstanceTransform:transform];
+    
+    _playerLayer = [AVPlayerLayer playerLayerWithPlayer:_player];
+    [_playerLayer setBounds:CGRectMake(0.0f, 0.0f, 700.0f, 300.0f)];
+    [_playerLayer setPosition:CGPointMake(0.0f, 0.0f)];
+    [_playerLayer setAnchorPoint:CGPointMake(0.0f, 0.0f)];
+    
+    [_replicatorLayer addSublayer:_playerLayer];
+    
+    [[[self view] layer] addSublayer:_replicatorLayer];
+    
+    [_player play];
 }
 
 @end
